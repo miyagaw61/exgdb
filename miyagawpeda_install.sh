e2a8f1f85fe3e330d1a023970d5cc3701222eee4
root <miyagaw61@gmail.com>
2017-04-30 10:49:21 +0900

    add_all

---
+++ miyagawpeda_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt-get install -y git  
+git clone https://github.com/longld/peda.git ~/peda  
+git clone https://github.com/miyagaw61/miyagawpeda.git ~/peda/miyagawpeda
+line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
+head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
+head -n $(cat ~/peda/peda.py | wc -l) ~/peda/peda.py | tail -n $(($(cat ~/peda/peda.py | wc -l) - ${line} + 1)) > ~/peda/miyagawpeda/peda.after.txt
+cp -a ~/peda/miyagawpeda/peda.before.txt ~/peda/miyagawpeda/miyagawpeda.py
+cat ~/peda/miyagawpeda/miyagawpeda.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+cat ~/peda/miyagawpeda/miyagawpeda.after.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+cat ~/peda/miyagawpeda/miyagawpeda.alias.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+cp -a ~/peda/miyagawpeda/miyagawpeda.py ~/peda/miyagawpeda.py
+rm -rf ~/peda/miyagawpeda/
+echo "source ~/peda/miyagawpeda.py" >> ~/.gdbinit
