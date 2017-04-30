9dc287b45d1699c64dedc4c153094e6cb24b2b1a
root <miyagaw61@gmail.com>
2017-04-30 11:03:58 +0900

    edit_update.sh

--- miyagawpeda_update.sh
+++ miyagawpeda_update.sh
@@ -5,7 +5,11 @@ line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
 head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
 head -n $(cat ~/peda/peda.py | wc -l) ~/peda/peda.py | tail -n $(($(cat ~/peda/peda.py | wc -l) - ${line} + 1)) > ~/peda/miyagawpeda/peda.after.txt
 cp -a ~/peda/miyagawpeda/peda.before.txt ~/peda/miyagawpeda/miyagawpeda.py
-cat ~/peda/miyagawpeda/miyagawpeda.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+if test "$(uname -a | grep 'x86_64')" ;then
+	cat ~/peda/miyagawpeda/miyagawpeda64.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+else
+	cat ~/peda/miyagawpeda/miyagawpeda32.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+fi
 cat ~/peda/miyagawpeda/miyagawpeda.after.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 cat ~/peda/miyagawpeda/miyagawpeda.alias.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 cp -a ~/peda/miyagawpeda/miyagawpeda.py ~/peda/miyagawpeda.py
