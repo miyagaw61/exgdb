235db01614ea92129a92895c94ff2145991d6055
root <miyagaw61@gmail.com>
2017-04-30 11:08:46 +0900

    edit_update.sh

--- miyagawpeda_update.sh
+++ miyagawpeda_update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf ~/peda/miyagawpeda.py
-git clone https://github.com/miyagaw61/miyagawpeda.git ~/peda/miyagawpeda
+#git clone https://github.com/miyagaw61/miyagawpeda.git ~/peda/miyagawpeda
 line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
 head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
 head -n $(cat ~/peda/peda.py | wc -l) ~/peda/peda.py | tail -n $(($(cat ~/peda/peda.py | wc -l) - ${line} + 1)) > ~/peda/miyagawpeda/peda.after.txt
