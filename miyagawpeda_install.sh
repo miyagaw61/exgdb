d716788b96247f104c12cc7b9a3e55812225e853
root <miyagaw61@gmail.com>
2017-05-04 19:55:17 +0900

    edit_install

--- miyagawpeda_install.sh
+++ miyagawpeda_install.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-apt-get install -y git  
 git clone https://github.com/longld/peda.git ~/peda  
-#git clone https://github.com/miyagaw61/miyagawpeda.git ~/peda/miyagawpeda
 line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
+mkdir ~/peda/miyagawpeda/
 head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
 head -n $(cat ~/peda/peda.py | wc -l) ~/peda/peda.py | tail -n $(($(cat ~/peda/peda.py | wc -l) - ${line} + 1)) > ~/peda/miyagawpeda/peda.after.txt
 cp -a ~/peda/miyagawpeda/peda.before.txt ~/peda/miyagawpeda/miyagawpeda.py
 if test "$(uname -a | grep 'x86_64')" ;then
-	cat ~/peda/miyagawpeda/miyagawpeda64.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+	cat miyagawpeda64.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 else
-	cat ~/peda/miyagawpeda/miyagawpeda32.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+	cat miyagawpeda32.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 fi
 cat ~/peda/miyagawpeda/miyagawpeda.after.txt >> ~/peda/miyagawpeda/miyagawpeda.py
-cat ~/peda/miyagawpeda/miyagawpeda.alias.txt >> ~/peda/miyagawpeda/miyagawpeda.py
-#rm -rf ~/peda/miyagawpeda/
-cat ~/peda/miyagawpeda/miyagawgdbinit >> ~/.gdbinit
+cat miyagawpeda.alias.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+cp miyagawgdbinit ~/.gdbinit
