22468d770b8b0f774e054f84c32f37daaa8c138f
root <miyagaw61@gmail.com>
2017-05-04 19:59:06 +0900

    edit_install

--- miyagawpeda_install.sh
+++ miyagawpeda_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-git clone https://github.com/longld/peda.git ~/peda  
+if test ! -e ~/peda/ ;then
+	git clone https://github.com/longld/peda.git ~/peda  
+fi
 line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
 mkdir ~/peda/miyagawpeda/
 head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
@@ -10,6 +12,6 @@ if test "$(uname -a | grep 'x86_64')" ;then
 else
 	cat miyagawpeda32.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 fi
-cat ~/peda/miyagawpeda/miyagawpeda.after.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+cat ~/peda/miyagawpeda/peda.after.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 cat miyagawpeda.alias.txt >> ~/peda/miyagawpeda/miyagawpeda.py
 cp miyagawgdbinit ~/.gdbinit
