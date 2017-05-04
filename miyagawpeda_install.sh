134dc4684af0e40bc9fa5ea9df981fb0e183da36
root <miyagaw61@gmail.com>
2017-05-04 20:08:36 +0900

    edit_install

--- miyagawpeda_install.sh
+++ miyagawpeda_install.sh
@@ -3,7 +3,9 @@ if test ! -e ~/peda/ ;then
 	git clone https://github.com/longld/peda.git ~/peda  
 fi
 line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
-mkdir ~/peda/miyagawpeda/
+if test ! -e ~/peda/miyagawpeda/ ;then
+	mkdir ~/peda/miyagawpeda/
+fi
 head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
 head -n $(cat ~/peda/peda.py | wc -l) ~/peda/peda.py | tail -n $(($(cat ~/peda/peda.py | wc -l) - ${line} + 1)) > ~/peda/miyagawpeda/peda.after.txt
 cp -a ~/peda/miyagawpeda/peda.before.txt ~/peda/miyagawpeda/miyagawpeda.py
