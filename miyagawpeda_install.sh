ef530b9640deb7a6e4e6793c6d7d6e15abf45287
miyagaw61 <bgtm1260295@gn.iwasaki.ac.jp>
2017-06-02 18:22:56 +0900

    cp peda/miyagawpeda/hoge.txt

--- miyagawpeda_install.sh
+++ miyagawpeda_install.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-if test ! -e ~/peda/ ;then
-	git clone https://github.com/longld/peda.git ~/peda  
+if test ! -e $HOME/peda/ ;then
+	git clone https://github.com/longld/peda.git $HOME/peda  
 fi
-line=$(cat ~/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
-if test ! -e ~/peda/miyagawpeda/ ;then
-	mkdir ~/peda/miyagawpeda/
+line=$(cat $HOME/peda/peda.py | grep -n "def xprint" | sed -E "s/(.*):.*:/\1/g")
+if test ! -e $HOME/peda/miyagawpeda/ ;then
+	mkdir $HOME/peda/miyagawpeda/
 fi
-head -n $((${line} - 1)) ~/peda/peda.py | tail -n $((${line} - 1)) > ~/peda/miyagawpeda/peda.before.txt
-head -n $(cat ~/peda/peda.py | wc -l) ~/peda/peda.py | tail -n $(($(cat ~/peda/peda.py | wc -l) - ${line} + 1)) > ~/peda/miyagawpeda/peda.after.txt
-cp -a ~/peda/miyagawpeda/peda.before.txt ~/peda/miyagawpeda/miyagawpeda.py
+cp -a "$(pwd)"/miyagawpeda64.txt $HOME/peda/miyagawpeda/
+cp -a "$(pwd)"/miyagawpeda32.txt $HOME/peda/miyagawpeda/
+cp -a "$(pwd)"/miyagawpeda.alias.txt $HOME/peda/miyagawpeda/
+head -n $((${line} - 1)) $HOME/peda/peda.py | tail -n $((${line} - 1)) > $HOME/peda/miyagawpeda/peda.before.txt
+head -n $(cat $HOME/peda/peda.py | wc -l) $HOME/peda/peda.py | tail -n $(($(cat $HOME/peda/peda.py | wc -l) - ${line} + 1)) > $HOME/peda/miyagawpeda/peda.after.txt
+cp -a $HOME/peda/miyagawpeda/peda.before.txt $HOME/peda/miyagawpeda/miyagawpeda.py
 if test "$(uname -a | grep 'x86_64')" ;then
-	cat miyagawpeda64.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+	cat $HOME/peda/miyagawpeda/miyagawpeda64.txt >> $HOME/peda/miyagawpeda/miyagawpeda.py
 else
-	cat miyagawpeda32.txt >> ~/peda/miyagawpeda/miyagawpeda.py
+	cat $HOME/peda/miyagawpeda/miyagawpeda32.txt >> $HOME/peda/miyagawpeda/miyagawpeda.py
 fi
-cat ~/peda/miyagawpeda/peda.after.txt >> ~/peda/miyagawpeda/miyagawpeda.py
-cat miyagawpeda.alias.txt >> ~/peda/miyagawpeda/miyagawpeda.py
-cp miyagawgdbinit ~/.gdbinit
+cat $HOME/peda/miyagawpeda/peda.after.txt >> $HOME/peda/miyagawpeda/miyagawpeda.py
+cat $HOME/peda/miyagawpeda/miyagawpeda.alias.txt >> $HOME/peda/miyagawpeda/miyagawpeda.py
+cp "$(pwd)"/miyagawgdbinit $HOME/.gdbinit
