6f85f6900f5d28a3e5f6aec1e2d3f9f08a16771f
miyagaw61 <miyagaw61@gmail.com>
2018-12-23 07:50:39 +0900

    Add source to peda_init.py

--- inits/peda_init.py
+++ inits/peda_init.py
@@ -1,6 +1,16 @@
-if os.path.exists("%s/expeda" % PLUGINPATH):
+is_expeda = os.path.exists("%s/expeda" % PLUGINPATH)
+is_peda = os.path.exists("%s/peda" % PLUGINPATH)
+is_pwngdb = os.path.exists("%s/Pwngdb" % PLUGINPATH)
+
+if is_expeda:
     gdb.execute("source %s/expeda/peda.py" % PLUGINPATH)
-elif os.path.exists("%s/peda" % PLUGINPATH):
+    if not is_pwngdb:
+        gdb.execute("source %s/exgdb_init.py" % INITS)
+elif is_peda:
     gdb.execute("source %s/peda/peda.py" % PLUGINPATH)
-if os.path.exists("%s/Pwngdb" % PLUGINPATH):
+    if not is_pwngdb:
+        gdb.execute("source %s/exgdb_init.py" % INITS)
+if is_pwngdb:
     gdb.execute("source %s/pwngdb_init_1.py" % INITS)
+else:
+    gdb.execute("source %s/exgdb_init.py" % INITS)
