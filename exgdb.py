14d104670d5d072a472dc95cad92e2adfcd51056
miyagaw61 <miyagaw61@gmail.com>
2019-09-04 17:33:10 +0900

    Clean

--- exgdb.py
+++ exgdb.py
@@ -10,7 +10,6 @@ import utils
 import exutils
 
 class BpRetHandler(gdb.FinishBreakpoint):
-
     def __init__(self, id_str, stop=False, fn=None):
         gdb.FinishBreakpoint.__init__(self, gdb.newest_frame(), internal=True)
         self.id = id_str
@@ -28,7 +27,6 @@ class BpRetHandler(gdb.FinishBreakpoint):
             return False
 
 class BpHandler(gdb.Breakpoint):
-
     def __init__(self, id_str, is_stop=False, ret=False, is_stop_ret=False, fn=None, ret_fn=None, silent=False):
         gdb.Breakpoint.__init__(self, id_str, type=gdb.BP_BREAKPOINT, internal=True)
         self.id = id_str
